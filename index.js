aba7fbfc2875c6dfdc2fbe60d7c0a58b4ebe6e18
vjcmacrury@gmail.com MacRury <vjcmacrury@gmail.com>
2018-05-19 19:36:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return(a + b)
+}
+
+function subtract (a,b) {
+  return (a - b)
+}
+
+function multiply (a, b) {
+  return (a * b)
+}
+
+function divide (a, b) {
+  return (a / b)
+}
+
+var n = 18
+
+function inc(n) {
+  return(n + 1)
+}
+
+function dec(n) {
+  return(n - 1)
+}
+
+function makeInt(a, b) {
+  return(a)
+} 
+
+function makeInt(0 * 2328) {
+  return(a)
+}
